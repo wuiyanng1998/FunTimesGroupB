0c7901f730db496584ba1a9fc41ea8964f9b2a38
zhasike <46502175+zhasike@users.noreply.github.com>
2019-03-04 12:02:35 +0000

    Adding php and adjusting makeNewbooking

--- php/makeNewBooking.php
+++ php/makeNewBooking.php
@@ -1,12 +1,16 @@
 <?php
 require_once('phpDatabaseConnection.php');
 
-$pickupDate = $_POST['$pickup_date'] ?? '1'; //PHP 7.0
-$pickupTime = $_POST['$pickup_time'] ?? '1';
-$numberOfPassengers = $_POST['number_passengers'] ?? '1';
-$numberOfLuggage = $_POST['luggage'] ?? '1';
-$carType = $_POST['car_type'] ?? '1';
+$pickupDate = $_POST['pickup_date'];
+$pickupTime = $_POST['pickup_time'];
+$numberOfPassengers = $_POST['number_passengers'];
+$numberOfLuggage = $_POST['luggage'];
+$carType = $_POST['car_type'];
 
+$start_address = $_POST['pickup_address'];
+$start_post_code = $_POST['pickup_address_api'];
+$end_address = $_POST['dropoff_address'];
+$end_post_code = $_POST['dropoff_address_api'];
 
 for ($i = 1; $i <= $numberOfPassengers; $i++) {
     ${"passengerName" . $i} = $_POST['passenger_name_' . $i];
@@ -45,6 +49,14 @@ switch ($carType) {
 //GET booker_id from COOKIES. We will get user_id here. We need then query booker_id
 $booker_id = 0;
 
+$qryAddRoute = "INSERT INTO route (start_address, start_post_code, end_address, end_post_code)";
+$qryAddRoute .= "VALUES ('" . $start_address . "', '" . $start_post_code . "', '" . $end_address . "', '"
+    . $end_post_code . ")";
+
+
+$qryGetLatestID = "SELECT LAST_INSERT_ID()";
+
+
 $qryAddBooking = "INSERT INTO booking (booking_time, vehicle_id, number_of_travelers, number_of_luggages, booker_id, ";
 $qryAddBooking .= "driver_id, service_fee, route_id) VALUES ('" . $pickupDateTime . "', '" . $carType . "', '"
     . $numberOfPassengers . "', '" . $numberOfLuggage . "', '" . $booker_id . ")";
@@ -53,14 +65,14 @@ $qryAddBooking .= "driver_id, service_fee, route_id) VALUES ('" . $pickupDateTim
 $qryFindTraveler = 0;
 //Add traveler query
 
+
 $qryFind = "SELECT * from film join film_actor on film.film_id = film_actor.film_id";
 $qryFind .= " JOIN actor on actor.actor_id= film_actor.actor_id";
 $qryFind .= " WHERE CONCAT( first_name, ' ', last_name) LIKE '%" . $search . "%'";
-$qryFind .=" OR  CONCAT(last_name, ' ', first_name)LIKE '%".$search . "%'
+$qryFind .= " OR  CONCAT(last_name, ' ', first_name)LIKE '%" . $search . "%'
         UNION
         SELECT * from film join film_actor on film.film_id = film_actor.film_id ";
-$qryFind .="JOIN actor on actor.actor_id= film_actor.actor_id WHERE title LIKE '%".$search . "%'";
-
+$qryFind .= "JOIN actor on actor.actor_id= film_actor.actor_id WHERE title LIKE '%" . $search . "%'";
 
 
 $qryDisableFK = "SET FOREIGN_KEY_CHECKS=0";
@@ -69,35 +81,19 @@ $qryEnableFK = "SET FOREIGN_KEY_CHECKS=1";
 
 $connection = connectToDb();
 
-mysqli_query($connection, $qryDisableFK);
+//mysqli_query($connection, $qryDisableFK);
 
 //Check if the name exists
-$result = mysqli_query($connection, $qryFind);
-if (mysqli_num_rows($result) > 0) {
-    session_start();
-    $output = "You successfully logged in";
-    $_SESSION['output'] = $output;
-    header('Location: loginResult.php');
 
+$result = mysqli_query($connection, $qryAddRoute);
+// check the query worked
+if ($result) {
+    closeDb($connection);
+//    header('Location: loginResult.php');
 } else {
-    $result = mysqli_query($connection, $qryAddBooking);
-    // check the query worked
-    if ($result) {
-        session_start();
-        $output = "We could not find your details. 
-        We created new account for you.";
-        $_SESSION['output'] = $output;
-        mysqli_query($connection, $qryEnableFK);
-        closeDb($connection);
-        header('Location: loginResult.php');
-    } else {
-        session_start();
-        $output = mysqli_error($connection);
-        $_SESSION['output'] = $output;
-        mysqli_query($connection, $qryEnableFK);
-        closeDb($connection);
-        header('Location: loginResult.php');
-        exit;
-    }
+
+    closeDb($connection);
+//    header('Location: loginResult.php');
+    exit;
 }
 
