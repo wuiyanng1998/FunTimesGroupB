c25fa2aefeb1daf556140f38451ce8965be79cdb
wuiyanng1998 <zceiwyn@ucl.ac.uk>
2019-03-25 15:28:10 +0800

    Completed

--- php/dbCredentials.php
+++ php/dbCredentials.php
@@ -1,6 +1,13 @@
 <?php
 define("DBSERVER","localhost");
-define("DBUSER","root");
-define("DBPASSWORD","");
+define("DBUSER","groupB");
+define("DBPASSWORD","travelBooking");
 define("DBNAME","booking");
+/* define("DBPORT",8889);*/
+
+
+//define("DBSERVER","localhost");
+//define("DBUSER","root);
+//define("DBPASSWORD","");
+//define("DBNAME","booking");
 /* define("DBPORT",8889);*/
